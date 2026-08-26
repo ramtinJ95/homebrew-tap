fa8773e21bacdad60d761c239db0af0318ef719f
ramtin <ramme95@gmail.com>
2026-08-26 09:59:42 +0200

    feat: add Macarchy formula

---
+++ Formula/macarchy.rb
@@ -0,0 +1,31 @@
+class Macarchy < Formula
+  desc "Theme-driven macOS environment"
+  homepage "https://github.com/ramtinJ95/macarchy"
+  url "https://github.com/ramtinJ95/macarchy/releases/download/v0.1.0/macarchy-0.1.0-arm64-apple-darwin.tar.gz"
+  sha256 "dc8d54201755103c6dc8b152b1abac38905e55edc3d709c04bcc85680c4ea337"
+  license "MIT"
+
+  depends_on arch: :arm64
+  depends_on macos: :tahoe
+  depends_on maximum_macos: :tahoe
+
+  def install
+    prefix.install "bin", "share"
+  end
+
+  test do
+    assert_equal "#{version}\n", shell_output("#{bin}/macarchy --version")
+
+    info = JSON.parse(shell_output("#{bin}/macarchy version --json"))
+    assert_equal version.to_s, info.fetch("version")
+    assert_equal "homebrew", info.fetch("installation")
+    assert_equal "macos-arm64", info.fetch("platform")
+    assert_match(/\A[0-9a-f]{40}\z/, info.fetch("revision"))
+
+    assert_equal <<~EOS, shell_output("#{bin}/macarchy theme list")
+      catppuccin-mocha\tdark\tCatppuccin Mocha
+      kanagawa-wave\tdark\tKanagawa Wave
+      tokyo-night\tdark\tTokyo Night
+    EOS
+  end
+end
--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # homebrew-tap
 
-Homebrew tap for [Archivist](https://github.com/ramtinJ95/archivist).
+Homebrew tap for [Archivist](https://github.com/ramtinJ95/archivist) and
+[Macarchy](https://github.com/ramtinJ95/macarchy).
 
 ## Install
 
 ```bash
 brew install ramtinJ95/tap/archivist
+brew install ramtinJ95/tap/macarchy
 ```
 
 ## Upgrade
 
 ```bash
 brew upgrade archivist
+brew upgrade macarchy
 ```
