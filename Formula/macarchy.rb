c27758e3638eeed94ffcd17707162d4617db40ec
ramtin <ramme95@gmail.com>
2026-08-29 18:29:27 +0200

    chore(macarchy): update to 0.4.1

--- Formula/macarchy.rb
+++ Formula/macarchy.rb
@@ -1,8 +1,8 @@
 class Macarchy < Formula
   desc "Theme-driven macOS environment"
   homepage "https://github.com/ramtinJ95/macarchy"
-  url "https://github.com/ramtinJ95/macarchy/releases/download/v0.4.0/macarchy-0.4.0-arm64-apple-darwin.tar.gz"
-  sha256 "c780158921de46c6a342ba1a1ccdb358fce9a95631c0aee4bc5d96250e4b9799"
+  url "https://github.com/ramtinJ95/macarchy/releases/download/v0.4.1/macarchy-0.4.1-arm64-apple-darwin.tar.gz"
+  sha256 "d83799e04a85346d1e0348cbffc8bc15458e90e1fc70217c852b7c44b656e207"
   license "MIT"
 
   depends_on arch: :arm64
